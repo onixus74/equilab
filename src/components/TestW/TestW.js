465cd297445f2b14a39bd1a39257b73a9553fab7
borgchrist1 <christian.borg@outlook.com>
2017-09-19 15:10:23 +0200

    test

--- src/components/TestW/TestW.js
+++ src/components/TestW/TestW.js
@@ -3,7 +3,6 @@ import TestC from '../TestC/TestC';
 
 export default class TestW extends Component {
     render () {
-        let b = null;
         return (
             <div className="bg">
                 <TestC />
