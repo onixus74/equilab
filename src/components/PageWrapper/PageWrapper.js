419a6c15fc42c05c714a13084635bf5918052b9d
borgchrist1 <christian.borg@outlook.com>
2017-10-10 12:16:49 +0200

    tech of the year

--- src/components/PageWrapper/PageWrapper.js
+++ src/components/PageWrapper/PageWrapper.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import IntroVideo from '../IntroVideo/IntroVideo';
 import './PageWrapper.css';
+import Button from "../Button";
 
 export default class PageWrapper extends Component {
     render () {
@@ -10,8 +11,13 @@ export default class PageWrapper extends Component {
                     <h1 className="caption">
                     Equilab - an app for the people who want the best for their horse.
                     </h1>
+                    <Button borderColor="white" hoverBgColor="white" textColor="white" hoverTextColor="black" text="Download"/>
                 </div>
                 <IntroVideo />
+                <div className="corner-right">
+                
+                    <div className="text-corner-right">Årets pryl</div>
+                </div>
             </div>
             
         );
